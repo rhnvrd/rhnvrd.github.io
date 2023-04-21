6250aeda7e70c2bcdd8c9c7e5386f2e13ef2e83c
Mahdi <mahdi.rhnvrd@gmail.com>
2023-04-21 15:19:21 +0330

    fresh start

---
+++ copy.sh
@@ -0,0 +1,14 @@
+clear
+
+
+echo "copy poole output to docs folder for github pages @ rhnvrd.github.io"
+
+cp -r ./output/ ./docs
+
+# data1="mahdi" 
+# data2="ali" 
+# data3="good"
+
+# echo "data1: $data1"
+# echo "data2: $data2"
+# echo "data3: $data3"
