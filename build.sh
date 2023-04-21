f70cd33c8f59a689e9f3f27492cabad60307abe3
Mahdi <mahdi.rhnvrd@gmail.com>
2023-04-21 15:40:52 +0330

    a

---
+++ build.sh
@@ -0,0 +1,16 @@
+clear
+
+
+
+python3 ../core/poole-tip/poole.py --build && cp -r ./output/ ./docs
+echo "\n\n build and copy poole output to docs folder for github pages @ rhnvrd.github.io \n\n"
+
+
+
+# data1="mahdi" 
+# data2="ali" 
+# data3="good"
+
+# echo "data1: $data1"
+# echo "data2: $data2"
+# echo "data3: $data3"
